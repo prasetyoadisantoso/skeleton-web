93e56ddb32549d073bbcf089fd103ef3c8394786
prasetyoadisantoso <prasetyoadisantoso@gmail.com>
2022-05-02 20:00:45 +0800

    Initial commit...

---
+++ README.md
@@ -0,0 +1,22 @@
+<img src="https://laravel.com/img/logomark.min.svg" width="50" height="50"><br />
+
+# Laravel Skeleton Web
+Simple Skeleton to Build Web App
+
+# Feature
+## Updated as needed
+- Unit Testing
+- Logging
+- Repositories
+- User & Roles
+- Multi-language
+
+# Requirement
+## Core
+- PHP 8^ and Extensions
+- Composer 
+- Web Server Apache / NGINX
+- DBMS like MySQL 
+
+## Additional
+- Under Construction
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Intervention
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Sven\ArtisanView\ServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
