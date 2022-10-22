530e56a43f71be2b4078a6a615fd3e89f3f163f8
satriyadi <satriyadi3001@gmail.com>
2022-10-22 13:49:12 +0800

    reset and forgot password layout entrydev

--- public/template/entrydev/assets/js/main.js
+++ public/template/entrydev/assets/js/main.js
@@ -189,3 +189,125 @@ $('#verification-submit').click(function () {
     }
 
 });
+
+// Registration
+$("#reset-password-form").validate({
+    rules: {
+        old_password: {
+            required: true,
+        },
+        new_password: {
+            required: true,
+        },
+        confirm_password: {
+            required: true,
+            equalTo: "#new_password",
+        }
+    },
+    messages: {
+        old_password: "<small style='color: red;'>password is required</small>",
+        new_password: "<small style='color: red;'>password is required</small>",
+        confirm_password: {
+            required: "<small style='color: red;'>password confirmation is required</small>",
+            equalTo: "<small style='color: red;'>password confirmation is must be same with new password</small>",
+        }
+    },
+    highlight: function (element) {
+        $(element).closest('.form-control').addClass('is-invalid');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-control').removeClass('is-invalid');
+    },
+    errorPlacement: function (error, element) {
+
+        if (element.attr("name") == "old_password") {
+            error.appendTo(".error-old-password");
+        }
+
+        if (element.attr("name") == "new_password") {
+            error.appendTo(".error-new-password");
+        }
+
+        if (element.attr("name") == "confirm_password") {
+            error.appendTo(".error-confirm");
+        }
+
+    },
+});
+
+$('#reset-password-submit').click(function () {
+    let a = $("#reset-password-form").valid();
+    if (a === true) {
+        $("#reset-password-form").submit();
+        Swal.fire({
+            title:"Password Updated",
+            text:"Password update successfully",
+            icon:'success',
+            customClass: {
+                popup: "rad-25",
+                confirmButton: "btn btn-success px-5 rad-25",
+            },
+            buttonsStyling: false,
+    });
+    } else {
+        Swal.fire({
+            title: "Password Update Failed",
+            text: "Exception Message",
+            icon: 'error'
+        });
+    }
+
+});
+
+// Forgot Password
+$("#forgot-password-form").validate({
+    rules: {
+        email: {
+            required: true,
+            email: true,
+        },
+    },
+    messages: {
+        email: {
+            required: "<small style='color: red;'>email is required</small>",
+            email: "<small style='color: red;'>must be contains email</small>"
+        },
+    },
+    highlight: function (element) {
+        $(element).closest('.form-control').addClass('is-invalid');
+    },
+    unhighlight: function (element) {
+        $(element).closest('.form-control').removeClass('is-invalid');
+    },
+    errorPlacement: function (error, element) {
+
+        if (element.attr("name") == "email") {
+            error.appendTo(".error-email");
+        }
+
+    },
+});
+
+$('#forgot-password-submit').click(function () {
+    let a = $("#forgot-password-form").valid();
+    if (a === true) {
+        $("#forgot-password-form").submit();
+        Swal.fire({
+            title:"Email Sent",
+            text:"Email reset password has been sent",
+            icon:'success',
+            customClass: {
+                popup: "rad-25",
+                confirmButton: "btn btn-success px-5 rad-25",
+            },
+            buttonsStyling: false,
+    });
+    } else {
+        Swal.fire({
+            title: "Email Not Sent",
+            text: "Exception Message",
+            icon: 'error'
+        });
+    }
+
+});
