0b1b7f949947b2f47c8ade10d60250c3af234a76
prasetyoadisantoso <prasetyoadisantoso@gmail.com>
2022-11-27 23:21:42 +0800

    Fixing dropdown sidebar

--- resources/views/template/default/dashboard/settings/meta/home.blade.php
+++ resources/views/template/default/dashboard/settings/meta/home.blade.php
@@ -2,7 +2,21 @@
 
 @section('meta-home')
 
-@include('template.default.dashboard.partial.breadcrumb')
+<!-- Start Breadcrumb -->
+<nav class="bg-light py-3 px-2 px-md-5 shadow-sm d-flex justify-content-between" aria-label="breadcrumb">
+    <h5 class="my-0"><i class="fa-solid fa-code me-3"></i>{{$breadcrumb['title']}}</h5>
+    <ol class="breadcrumb my-0">
+        <li class="breadcrumb-item"><a href="{{route($url)}}"
+                class="text-decoration-none text-dark">{{$breadcrumb['home']}}</a>
+        </li>
+        <li class="breadcrumb-item active text-muted" aria-current="page">
+            {{$type == 'index' ? $breadcrumb['index'] : ''}}
+            {{$type == 'create' ? $breadcrumb['create'] : ''}}
+            {{$type == 'edit' ? $breadcrumb['edit'] : ''}}
+        </li>
+    </ol>
+</nav>
+<!-- End Breadcrumb -->
 
 <!-- Start Home -->
 <div class="container py-2">
