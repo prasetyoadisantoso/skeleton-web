3eeddb5af13fad93ccbfa10f6bcc7cff3cdc8b38
prasetyoadisantoso <prasetyoadisantoso@gmail.com>
2022-12-22 12:25:13 +0800

    Update package and minor fixing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -105,6 +105,78 @@
         </div>
     </div>
 
+    <!-- Menu Mobile Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header border-bottom-0">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body mx-5" id="menu-modal">
+                    <ul class="list-group border-0">
+                        @if (Auth::user() == null)
+                        <li class="list-group-item my-3 border-0"><a class="mx-2 text-decoration-none text-dark"
+                                href="{{route('login.page')}}"><i
+                                    class="fa-solid fa-right-to-bracket mx-2"></i>{{__('welcome.header.sign_in')}}</a>
+                        </li>
+                        <li class="list-group-item my-3 border-0"><a class="mx-2 text-decoration-none text-dark"
+                                href="{{route('register.page')}}"><i
+                                    class="fa-solid fa-user-plus mx-2"></i>{{__('welcome.header.sign_up')}}</a>
+                        </li>
+                        @else
+                        @can('main-index')
+                        <li class="list-group-item my-3 border-0"><a href="{{route('dashboard.main')}}"
+                                class="text-dark"><i
+                                    class="fa-solid fa-gauge me-3"></i>{{__('welcome.header.dashboard')}}</a></li>
+                        @endcan
+                        <li class="list-group-item my-3 border-0"><a href="{{route('logout')}}" class="text-dark"><i
+                                    class="fa-solid fa-right-from-bracket me-3"></i>{{__('welcome.header.logout')}}</a>
+                        </li>
+                        </li>
+                        @endif
+                    </ul>
+                    <div class="container border-0 my-4">
+                        <p>
+                            @if (LaravelLocalization::getCurrentLocale() == 'id')
+                            <a class="text-dark text-decoration-none dropdown-toggle" data-bs-toggle="collapse"
+                                href="#collapseExample" role="button" aria-expanded="false"
+                                aria-controls="collapseExample">
+                                <span class="fi fi-id me-3"></span>Indonesia
+                            </a>
+
+                            @elseif (LaravelLocalization::getCurrentLocale() == 'en')
+                            <a class="text-dark text-decoration-none dropdown-toggle" data-bs-toggle="collapse"
+                                href="#collapseExample" role="button" aria-expanded="false"
+                                aria-controls="collapseExample">
+                                <span class="fi fi-gb me-3"></span>English
+                            </a>
+
+                            @else
+                            <span class="fi fi-jp mr-2"></span>
+                            <span class="text-muted text-sm d-md-inline">Japan</span>
+
+                            @endif
+                        </p>
+                        <div class="collapse lang-collapse" id="collapseExample">
+                            <div class="container rounded-2 card py-3">
+                                <li><a class="dropdown-item px-2 py-2"
+                                        href="{{LaravelLocalization::getLocalizedURL('id')}}"><span
+                                            class="fi fi-id me-3"></span>Indonesia</a>
+                                </li>
+                                <li><a class="dropdown-item px-2 py-2"
+                                        href="{{LaravelLocalization::getLocalizedURL('en')}}"></span> <span
+                                            class="fi fi-gb me-3"></span>English</a></li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer my-3 border-0">
+                    <!-- Footer Code -->
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Main Page -->
     <div class="containerize">
 
@@ -252,77 +324,6 @@
                             <small>Front-end Developer</small>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Menu Mobile Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header border-bottom-0">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body mx-5" id="menu-modal">
-                        @if (Auth::user() == null)
-                        <li class="list-group-item my-3 border-0"><a class="mx-2 text-decoration-none text-dark"
-                                href="{{route('login.page')}}"><i
-                                    class="fa-solid fa-right-to-bracket mx-2"></i>{{__('welcome.header.sign_in')}}</a>
-                        </li>
-                        <li class="list-group-item my-3 border-0"><a class="mx-2 text-decoration-none text-dark"
-                                href="{{route('register.page')}}"><i
-                                    class="fa-solid fa-user-plus mx-2"></i>{{__('welcome.header.sign_up')}}</a>
-                        </li>
-                        @else
-                        @can('main-index')
-                        <li class="list-group-item my-3 border-0"><a href="{{route('dashboard.main')}}"
-                                class="text-dark"><i
-                                    class="fa-solid fa-gauge me-3"></i>{{__('welcome.header.dashboard')}}</a></li>
-                        @endcan
-                        <li class="list-group-item my-3 border-0"><a href="{{route('logout')}}" class="text-dark"><i
-                                    class="fa-solid fa-right-from-bracket me-3"></i>{{__('welcome.header.logout')}}</a>
-                        </li>
-                        </li>
-                        @endif
-                    </ul>
-                    <div class="container border-0 my-3">
-                        <p>
-                            @if (LaravelLocalization::getCurrentLocale() == 'id')
-                            <a class="text-dark text-decoration-none dropdown-toggle" data-bs-toggle="collapse"
-                                href="#collapseExample" role="button" aria-expanded="false"
-                                aria-controls="collapseExample">
-                                <span class="fi fi-id me-3"></span>Indonesia
-                            </a>
-
-                            @elseif (LaravelLocalization::getCurrentLocale() == 'en')
-                            <a class="text-dark text-decoration-none dropdown-toggle" data-bs-toggle="collapse"
-                                href="#collapseExample" role="button" aria-expanded="false"
-                                aria-controls="collapseExample">
-                                <span class="fi fi-gb me-3"></span>English
-                            </a>
-
-                            @else
-                            <span class="fi fi-jp mr-2"></span>
-                            <span class="text-muted text-sm d-md-inline">Japan</span>
-
-                            @endif
-                        </p>
-                        <div class="collapse lang-collapse" id="collapseExample">
-                            <div class="container rounded-2 card py-3">
-                                <li><a class="dropdown-item px-2 py-2"
-                                        href="{{LaravelLocalization::getLocalizedURL('id')}}"><span
-                                            class="fi fi-id me-3"></span>Indonesia</a>
-                                </li>
-                                <li><a class="dropdown-item px-2 py-2"
-                                        href="{{LaravelLocalization::getLocalizedURL('en')}}"></span> <span
-                                            class="fi fi-gb me-3"></span>English</a></li>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer my-3 border-0">
-                    <!-- Footer Code -->
                 </div>
             </div>
         </div>
