eebf3de7fc9f34d7fa5774be46a30442aa15e4f2
prasetyoadisantoso <prasetyoadisantoso@gmail.com>
2023-07-24 15:08:57 +0800

    Adding SEO section

--- resources/views/template/default/frontend/layout/main.blade.php
+++ resources/views/template/default/frontend/layout/main.blade.php
@@ -7,6 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="{{$site_favicon}}" />
     <title>Skeleton Web</title>
+
+    {{-- SEO --}}
+    @include('template.default.frontend.section.seo')
+
     <!-- Bootstrap only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
---
+++ resources/views/template/default/frontend/section/seo.blade.php
@@ -0,0 +1,11 @@
+{{-- Meta Tag --}}
+<meta name="description" content="Description of your web page here.">
+<meta name="robots" content="index, follow">
+
+{{-- OpenGraph --}}
+<meta property="og:title" content="Your Page Title Here">
+<meta property="og:description" content="Description of your web page here.">
+<meta property="og:image" content="URL of the image you want to display when shared">
+<meta property="og:url" content="URL of the web page">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Your Site Name">
